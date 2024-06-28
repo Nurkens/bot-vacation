3e52bb6c30bef381fdc88e442736298efaa80043
Nurken <nurken.serdakov.2000@mail.ru>
2024-06-28 22:33:20 +0600

    прогноз погоды

--- index.js
+++ index.js
@@ -1,57 +1,41 @@
 const TelegramBot = require('node-telegram-bot-api');
+const { gameOptions, againOptions } = require('./options.js');
 
+const weather_key = '86c33f2ccde9c230d73b0d3da5cae232';
 const token = '7382508718:AAEDXTrjB14AuetMaRLRijSFiibG6SauL5c'; 
 
 const bot = new TelegramBot(token, { polling: true });
 
 const chats = {};
 
-
-const startGame = async (chatId) =>{
-    await bot.sendMessage(chatId, `Сейчас я загадаю цифру от 0 до 9, а ты должен ее отгадать`);
-    const randomNumber = Math.floor(Math.random() * 10);
-    chats[chatId] = randomNumber;
-    await bot.sendMessage(chatId, 'Отгадай', gameOptions);
-}
-
-const gameOptions = {
-  reply_markup: JSON.stringify({
-    inline_keyboard: [
-      [{ text: '1', callback_data: '1' }, { text: '2', callback_data: '2' }, { text: '3', callback_data: '3' }],
-      [{ text: '4', callback_data: '4' }, { text: '5', callback_data: '5' }, { text: '6', callback_data: '6' }],
-      [{ text: '7', callback_data: '7' }, { text: '8', callback_data: '8' }, { text: '9', callback_data: '9' }],
-      [{ text: '0', callback_data: '0' }],
-    ]
-  })
+const startGame = async (chatId) => {
+  await bot.sendMessage(chatId, 'Сейчас я загадаю цифру от 0 до 9, а ты должен ее отгадать');
+  const randomNumber = Math.floor(Math.random() * 10);
+  chats[chatId] = randomNumber;
+  await bot.sendMessage(chatId, 'Отгадай', gameOptions);
 };
-const  againOptions = {
-    reply_markup: JSON.stringify({
-      inline_keyboard: [
-        [{ text: 'Играть еще раз', callback_data: '/again' }],
-      ]
-    })
-  };
+
+bot.setMyCommands([
+  { command: '/start', description: 'Приветствие' },
+  { command: '/info', description: 'Получить инфо о пользователе' },
+  { command: '/game', description: 'Игра отгадай цифру' },
+  { command: '/weather', description: 'чтобы узнать погоду,напиши /weather и название города' },
+]);
 
 const start = () => {
-  bot.setMyCommands([
-    { command: '/start', description: 'greeting' },
-    { command: '/info', description: 'get info about user' },
-    { command: '/game', description: 'game guess the number' },
-  ]);
-
   bot.onText(/\/start/, (msg) => {
     const chatId = msg.chat.id;
-    bot.sendMessage(chatId, 'Добро пожаловать! Это ваш новый Telegram-бот.');
+    bot.sendMessage(chatId, 'Привет,со мной ты можешь поиграть в игру,узнать погоду');
   });
 
   bot.onText(/\/info/, (msg) => {
     const chatId = msg.chat.id;
-    bot.sendMessage(chatId, `Тебя зовут ${msg.from.first_name} ${msg.from.last_name}`);
+    bot.sendMessage(chatId, `Тебя зовут ${msg.from.first_name} sugar daddy:)`);
   });
 
   bot.onText(/\/game/, (msg) => {
     const chatId = msg.chat.id;
-    bot.sendMessage(chatId, `Сейчас я загадаю цифру от 0 до 9, а ты должен ее отгадать`);
+    bot.sendMessage(chatId, 'Сейчас я загадаю цифру от 0 до 9, а ты должен ее отгадать');
     const randomNumber = Math.floor(Math.random() * 10);
     chats[chatId] = randomNumber;
     return bot.sendMessage(chatId, 'Отгадай', gameOptions);
@@ -61,21 +45,50 @@ const start = () => {
     const data = msg.data;
     const chatId = msg.message.chat.id;
 
-    if(data === '/again'){
-        return startGame(chatId);
+    if (data === '/again') {
+      return startGame(chatId);
     }
 
     if (data == chats[chatId]) {
-        return bot.sendMessage(chatId, `Поздравляю! Ты отгадал цифру ${chats[chatId]}`,againOptions);
+      return bot.sendMessage(chatId, `Поздравляю! Ты отгадал цифру ${chats[chatId]}`, againOptions);
     } else {
-      return bot.sendMessage(chatId, `Ты не угадал, я загадал цифру ${chats[chatId]}`,againOptions);
+      return bot.sendMessage(chatId, `Ты не угадал, я загадал цифру ${chats[chatId]}`, againOptions);
     }
-    
-    
+  });
+
+  const getWeather = async (city) => {
+    const fetch = await import('node-fetch').then((module) => module.default);
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${weather_key}&units=metric&lang=ru`;
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+
+      console.log(data); 
+
+      if (data.cod !== 200) {
+        return 'Город не найден.';
+      }
+      const chatId = msg.chat.id;
+      const weather = data.weather[0].description;
+      const temp = data.main.temp;
+      const windspeed = data.wind.speed;
+      return `Погода в городе ${city}:\nТемпература: ${temp}°C\nОписание: ${weather} \nСкорость ветра ${windspeed} м/с`;
+      
+    } catch (error) {
+      console.error('Error fetching weather data:', error);
+      return 'Ошибка получения данных о погоде.';
+    }
+  };
+
+  bot.onText(/\/weather (.+)/, async (msg, match) => {
+    const chatId = msg.chat.id;
+    const city = match[1];
+    const weatherReport = await getWeather(city);
+    bot.sendMessage(chatId, weatherReport);
   });
 
   bot.on('polling_error', (error) => {
-    console.log(error.code);  
+    console.log(error.code);
   });
 };
 
